ddcca285a788c598977add8e2a3bb4530486fe99
gabiezzi <geiezzi90@gmail.com>
2023-07-04 20:27:13 -0300

    add Project structure, H2 database config

---
+++ backend/src/test/java/com/nocountry/movenow/MovenowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.movenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovenowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
