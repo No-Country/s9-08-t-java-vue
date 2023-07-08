568a597cdd60d2b6eda02877694024d45e9a6bb2
Mayder C <44930667+MayderC@users.noreply.github.com>
2023-07-08 03:43:53 -0600

    feat: add footer, responsive

---
+++ frontend/src/components/common/MNFooter.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="text-blue-night p-7 md:px-24 md:py-16">
+    <div class="flex justify-between flex-wrap">
+      <div class="flex lg:w-2/3 md:gap-32 flex-col md:flex-row gap-10">
+        <div>
+          <h2 class="text-lg font-semibold">MoveNow</h2>
+          <p>Nosotros</p>
+          <p>Nuestra Flota</p>
+        </div>
+        <div>
+          <h2 class="text-lg font-semibold">Informaciones al consumidor</h2>
+          <p>MoveNow SA</p>
+          <P>N 0000000000000000000</P>
+          <p>Sede: Av. Bernardo Vasconcelos, n° 377 - CABA</p>
+          <p>Central de Reservas y Asistente para clientes 24hs:</p>
+          <p>0800 979 2020</P>
+          <p>E-mail: centraldereservasmn@gmail.com</p> 
+        </div>
+      </div>
+      <div class="mt-10 md:mt-0">
+          <h2 class="text-lg font-semibold">Contacto</h2>
+          <div class="flex gap-2 my-4">
+            <img class="w-11" src="@/assets/img/f-icon-ig.svg" alt="">
+            <img class="w-11" src="@/assets/img/f-icon-fb.svg" alt="">
+            <img class="w-11" src="@/assets/img/f-icon-tw.svg" alt="">
+          </div>
+          <p>Privacidad</p>
+          <p>Terminos y condiciones</p>
+          <p>&copy; 2023 MoveNow</p>
+      </div>
+    </div>
+    <div class="mt-9">
+      <p>MoveNow - Todos los derechos reservados</p>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts" setup></script>
