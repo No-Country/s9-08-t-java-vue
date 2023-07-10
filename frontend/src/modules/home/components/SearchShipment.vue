1f9ca2b964ff3e8300127a9442a7c47a34e7103e
arbelais <arbelaism@outlook.com>
2023-07-10 18:57:46 -0300

    feat: PB-37 add shipment form

---
+++ frontend/src/modules/home/components/SearchShipment.vue
@@ -0,0 +1,17 @@
+<template>
+  <form class="w-full rounded-4xl bg-white shadow-2xl">
+    <MNDropdownInput @selected="setType" :items="['Mudanza']" :text="typeSelected" />
+
+    <label for=""></label>
+    <MNInput placeholder="Origen" type="text" />
+  </form>
+</template>
+
+<script setup lang="ts">
+import MNDropdownInput from '@/components/common/MNDropdownInput.vue'
+import MNInput from '@/components/common/MNInput.vue'
+import { ref } from 'vue'
+
+const typeSelected = ref('Tipo de envio')
+const setType = (type: string) => (typeSelected.value = type)
+</script>
--- frontend/src/modules/home/views/HomeView.vue
+++ frontend/src/modules/home/views/HomeView.vue
@@ -1,14 +1,14 @@
 <template>
-  <main>
-    <h1>Home</h1>
-    <MNToggle :color="'bg-green-400'" @state="isChecked"></MNToggle>
+  <main class="font-montserrat">
+    <HeroHome />
+    <SearchShipment />
   </main>
 </template>
 
 <script setup lang="ts">
-
-import MNToggle from '@/components/common/MNToggle.vue';
+import MNToggle from '@/components/common/MNToggle.vue'
+import HeroHome from '../components/HeroHome.vue'
+import SearchShipment from '../components/SearchShipment.vue'
 
 const isChecked = (value: boolean) => console.log(value)
-
 </script>
