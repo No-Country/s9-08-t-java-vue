741bd130a1178b587c27aec4446c536b9e397fdf
arbelais <arbelaism@outlook.com>
2023-07-12 04:31:21 -0300

    fix: set unique key in FleetCard

--- frontend/src/modules/home/data/flota-info.ts
+++ frontend/src/modules/home/data/flota-info.ts
@@ -1,26 +1,29 @@
 export const flota = [
   {
-    "name": "CAMIONETA PICK UP",
-    "short": "S10 CABINA SIMPLE",
-    "info1": "Capacidad de carga hasta de 1000kg",
-    "info2": "Ideal para distancias cortas y medias.",
-    "selectable": false,
-    "img": "./src/assets/img/camioneta.webp"
+    id: 1,
+    name: 'CAMIONETA PICK UP',
+    short: 'S10 CABINA SIMPLE',
+    info1: 'Capacidad de carga hasta de 1000kg',
+    info2: 'Ideal para distancias cortas y medias.',
+    selectable: false,
+    img: './src/assets/img/camioneta.webp'
   },
   {
-    "name": "CAMION",
-    "short": "S10 CABINA SIMPLE",
-    "info1": "Capacidad de carga hasta de 1000kg",
-    "info2": "Ideal para distancias cortas y medias.",
-    "selectable": false,
-    "img": "/src/assets/img/camion.webp"
+    id: 2,
+    name: 'CAMION',
+    short: 'S10 CABINA SIMPLE',
+    info1: 'Capacidad de carga hasta de 1000kg',
+    info2: 'Ideal para distancias cortas y medias.',
+    selectable: false,
+    img: '/src/assets/img/camion.webp'
   },
   {
-    "name": "CAMION",
-    "short": "S10 CABINA SIMPLE",
-    "info1": "Capacidad de carga hasta de 1000kg",
-    "info2": "Ideal para distancias cortas y medias.",
-    "selectable": false,
-    "img": "/src/assets/img/camion-2.webp"
+    id: 3,
+    name: 'CAMION',
+    short: 'S10 CABINA SIMPLE',
+    info1: 'Capacidad de carga hasta de 1000kg',
+    info2: 'Ideal para distancias cortas y medias.',
+    selectable: false,
+    img: '/src/assets/img/camion-2.webp'
   }
 ]
