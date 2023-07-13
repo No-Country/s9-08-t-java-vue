8d344e05730f0ea2f6a28a775373e887bf78da25
Mayder C <44930667+MayderC@users.noreply.github.com>
2023-07-13 01:50:49 -0600

    feat: add axios, login request

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,10 @@
 <template>
   <RouterView />
 </template>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { useAuthStore } from './modules/auth/store/auth'
+
+const store = useAuthStore()
+onMounted(() => store.setAuthProfile())
+</script>
