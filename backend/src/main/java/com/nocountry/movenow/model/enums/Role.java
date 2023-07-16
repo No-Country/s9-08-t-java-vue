eb01e45378bacc90869f78d316d5897f574ff8ab
Daniel Matos <deme12.1@gmail.com>
2023-07-15 21:34:53 -0500

    Create User CRUD. PUT does not still works

--- backend/src/main/java/com/nocountry/movenow/model/enums/Role.java
+++ backend/src/main/java/com/nocountry/movenow/model/enums/Role.java
@@ -1,7 +1,22 @@
 package com.nocountry.movenow.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.persistence.Column;
+
 public enum Role {
 
-    USER ,
-    ADMIN
+    USER ("User"),
+    ADMIN ("Admin");
+
+    @Column(name = "name")
+    private String name;
+    Role(String name) {
+        this.name = name;
+    }
+
+    @JsonValue
+    public String getName() {
+        return name;
+    }
 }
