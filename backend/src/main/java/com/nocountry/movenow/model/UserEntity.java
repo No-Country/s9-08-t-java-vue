5795fe0c2f565176a74af9911e21b22bac7f6f79
Daniel Matos <deme12.1@gmail.com>
2023-07-16 23:30:16 -0500

    Add column name to id_moving in Comment

--- backend/src/main/java/com/nocountry/movenow/model/UserEntity.java
+++ backend/src/main/java/com/nocountry/movenow/model/UserEntity.java
@@ -47,7 +47,6 @@ public class UserEntity implements UserDetails {
         return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
-
     @Override
     public String getUsername() {
         return username;
