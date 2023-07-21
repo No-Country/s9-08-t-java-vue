a88935c5cc92ef2713f94c9d3c54986c4db3b7e7
Mayder C <44930667+MayderC@users.noreply.github.com>
2023-07-21 14:10:21 -0600

    feat: last steps, moving views

--- frontend/src/modules/moving/views/MovingView.vue
+++ frontend/src/modules/moving/views/MovingView.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="container mx-auto overflow-x-hidden p-2 font-montserrat">
-    <StepComponent :values="stepComponentData"></StepComponent>
+    <StepComponent :values="stepComponentData" @set:current="onEdit"></StepComponent>
     <!--TODO: Move section to component stepOne-->
     <section v-if="stepForm == 1">
       <MovingType />
@@ -9,11 +9,17 @@
       <TimeComponent />
     </section>
     <!--TODO: Move section to component stepOne-->
-    <section v-if="stepForm == 2"></section>
-    <section v-if="stepForm == 3"></section>
-    <section v-if="stepForm == 4"></section>
+    <section v-if="stepForm == 2">
+      <Payment></Payment>
+    </section>
+    <section v-if="stepForm == 3">
+      <OrderVerification></OrderVerification>
+    </section>
+    <section v-if="stepForm == 4">
+      <OrderFinished />
+    </section>
 
-    <section class="flex justify-end gap-1">
+    <section v-if="(stepForm == 4) == false" class="flex justify-end gap-1">
       <MNButton
         text="Atras"
         :class="`w-36 cursor-pointer rounded-lg bg-primary-orange py-1 ${
@@ -39,15 +45,21 @@ import ExtrasComponent from '../components/ExtrasComponent.vue'
 import TimeComponent from '../components/TimeComponent.vue'
 import MovingType from '../components/MovingType.vue'
 import MNButton from '@/components/common/MNButton.vue'
-import { ref } from 'vue'
+import OrderFinished from '../components/OrderFinished.vue'
+import OrderVerification from '../components/OrderVerification.vue'
+import Payment from '../components/Payment.vue'
 
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 const stepForm = ref(1)
 const stepComponentData = ref([
   { editable: true, name: 'Tipo de envío', status: true, stepNumber: 0 },
   { editable: true, name: 'Especificaciones del pedido', status: false, stepNumber: 1 },
   { editable: true, name: 'Pago', status: false, stepNumber: 2 },
   { editable: true, name: 'Verificación de pedido', status: false, stepNumber: 3 },
-  { editable: true, name: 'Pedido Finalizado', status: false, stepNumber: 4 }
+  { editable: false, name: 'Pedido Finalizado', status: false, stepNumber: 4 }
 ])
 
 const nextStep = () => {
@@ -58,5 +70,11 @@ const nextStep = () => {
 const prevStep = () => {
   stepForm.value--
   stepComponentData.value[stepForm.value].status = false
+}
+
+const onEdit = (index: number) => {
+  if (index == 0) router.push('/')
+  stepComponentData.value[index].status = false
+  stepForm.value = index
 }
 </script>
