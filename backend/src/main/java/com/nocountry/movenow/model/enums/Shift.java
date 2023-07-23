2d424559aef086802e6ab8799b67d1c09c66387f
Daniel Matos <deme12.1@gmail.com>
2023-07-23 14:50:47 -0500

    Adapt Shift to retrieve time information when is called by the /api/schedule/available-shifts endpoint

--- backend/src/main/java/com/nocountry/movenow/model/enums/Shift.java
+++ backend/src/main/java/com/nocountry/movenow/model/enums/Shift.java
@@ -1,24 +1,26 @@
 package com.nocountry.movenow.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Shift {
-    First("First", "08:00h - 12:00h"),
-    Second("Second", "13:00h - 17:00h");
+    First(1, "Primer turno: 08:00h - 12:00h"),
+    Second(2, "Segundo turno: 13:00h - 17:00h");
 
-    private String name;
-    private String time;
+    private final int shift;
+    private final String time;
 
-    Shift(String name, String time) {
-        this.name = name;
+    Shift(int shift, String time) {
+        this.shift = shift;
         this.time = time;
     }
 
-    @JsonValue
-    public String getName() {
-        return name;
+    @JsonCreator
+    public int getShift() {
+        return shift;
     }
 
+    @JsonValue
     public String getTime() {
         return time;
     }
